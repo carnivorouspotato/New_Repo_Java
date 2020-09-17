40b2808714724c8fdd4f66fc3c0bafeab5e4a80d
David Smith <davidsmith@Davids-iMac.local>
2020-09-16 19:40:21 -0600

    MyHomeWork1

---
+++ src/HomeWork1.java
@@ -0,0 +1,26 @@
+public class HomeWork1 {
+
+
+    public static void main(String[] args) {
+        byte noOfHoursDay = 24;
+        System.out.println("There are " + noOfHoursDay + " hours in a day.");
+        int noOfDaysYear = 365;
+        System.out.println("There are " + noOfDaysYear + " days a year.");
+        int noOfEmployees = 5000;
+        System.out.println("There are " + noOfEmployees + " employees at my office.");
+        int coPopulation = 54821;
+        System.out.println("There are " + coPopulation + " residence in Saint Kitts and Nevis.");
+        double interestRate = 4.75;
+        System.out.println("Current automotive interest rate is " + interestRate + ".");
+        double bankBalance = 6000.36;
+        System.out.println("My current bank account balance is $" + bankBalance + ".");
+        boolean sunriseWest = false;
+        String result;
+        result = (sunriseWest == false) ? "No" : "Yes";
+        System.out.println("Does the sun rise from the west? " + result);
+        //Ternary Operator
+        String firstName = "D";
+        String lastName = "S";
+        System.out.println("Initials of my name are: " + firstName + lastName);
+    }
+}
