475c198734eba0e88d189b07a63761c96dfd9b16
David Smith <davidsmith@Davids-iMac.local>
2020-10-12 16:43:18 -0600

    MathLibrary.java

--- src/Calculations.java
+++ src/Calculations.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Calculations {
+public class Calculations {
+    public static void main(String[] args) {
+
+        MathLibrary m3 = new MathLibrary();
+        String revString = "hello";
+        String revString2 = m3.reverseString(revString);
+        System.out.println(revString2);
+
+
+        MathLibrary newLibrary = new MathLibrary();
+        double[] testArray = {22.5, 65.35, 55.4, 85.9};
+        System.out.println(newLibrary.arrOne(testArray));
+    }
 }
--- src/MathLibrary.java
+++ src/MathLibrary.java
@@ -1,20 +1,52 @@
 public class MathLibrary {
-    public int addition (int num1, int num2 ){
+    /**public int addition(int num1, int num2) {
         return num1 + num2;
 
     }
-    public int subtwo ( int num1, int num2){
+
+   /** public int subtwo(int num1, int num2) {
         return num1 - num2;
 
     }
-    public int multiply (int num1, int num2){
+
+    /**public int multiply(int num1, int num2) {
         return num1 * num2;
     }
-   public double dividetwo (double num1, int num2){
+
+   /** public double dividetwo(double num1, int num2) {
         double result = -1.00;
-        if (num2 != 0){
-            result = num1/num2;
+        if (num2 != 0) {
+            result = num1 / num2;
         }
         return result;
-   }
+    }
+
+    /**public int primeTest(int prime) {
+        int num = 0;
+        boolean flag = false;
+        for (int i = 0; i <= num / 2; i++) {
+            if (num % i == 0) {
+                flag = true;
+            }
+
+        }
+        return prime;*/
+    
+
+    public double arrOne(double[] arr) {
+        double sum = -1;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        return sum;
+    }
+
+    public String reverseString(String str) {
+        String res = "";
+
+        for (int i = str.length() - 1; i <= 0; i--) {
+            res = res + str.charAt(i);
+        }
+        return res;
+    }
 }
