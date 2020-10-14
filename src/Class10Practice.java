70fc5ef846ed45db9483cf4f2a8fa74b822854ce
David Smith <davidsmith@Davids-iMac.local>
2020-10-13 21:33:53 -0600

    Class10.java

--- src/Class10Practice.java
+++ src/Class10Practice.java
@@ -1,35 +1,15 @@
 public class Class10Practice {
 
-
-
-
     public static void main(String[] args) {
         Class10 printingMethods = new Class10();
-         int test2 = printingMethods.usedToner(91);
-        for (int i = 0 ; i <= Class10.maxToner ; i--){
-            if (test2 <= Class10.maxToner && test2 >= 10){
-                System.out.println("You have " + test2 + " % toner left");
-            break;
-
-
-            }
-            if (test2 <= 10){
-                System.out.println("Toner is low");
-                break;
-            }
-        }
-        int test3 = printingMethods.usedPaper(0);
+         int singlePageToner = printingMethods.usedToner(91);
+        int singPagePaper = printingMethods.usedPaper(99);
 
 
 
 
-        int test5 = printingMethods.twoSided(15);
-        int test6 =printingMethods.twoSidedTonerUse(55);
-        int test7 = printingMethods.addedPaper(55);
-        System.out.println("test6 "+ test6);
 
-int test10 = test6 + test2;
-        System.out.println("test10 " + test10);
+
 
 
 
