cd2555389cafb2344ebfb4c902594d03f2db07e9
David Smith <davidsmith@Davids-iMac.local>
2020-10-13 21:22:55 -0600

    Class10.java

--- src/Class10.java
+++ src/Class10.java
@@ -1,47 +1,104 @@
 import javax.swing.*;
 
 public class Class10 {
-    public int paperAmount = 100;
-    public int maxToner = 100;
+    public static int paperAmount = 100;
+    public static int maxToner = 100;
 
-    public int usedToner(int tonerUsed) {
-        int level = 0;
+
+    public int usedToner(int tonerUsedSingle ) {
+        int levelTonerUsed = 0;
         for (int i = 0; i <= maxToner; i--) {
-            level = maxToner - tonerUsed;
+            levelTonerUsed = maxToner - tonerUsedSingle;
 
-            if (level == maxToner) {
+            if (levelTonerUsed == maxToner) {
                 System.out.println("Toner is Full");
                 break;
             } else {
-                System.out.println("You have " + level + "% toner left");
+                System.out.println("You have " + levelTonerUsed + "% toner left");
                 break;
             }
 
 
         }
-        return level;
+        return levelTonerUsed;
 
     }
 
     public int usedPaper(int paperUsed) {
-        int level = 0;
-        for (int i = 0; i <= paperAmount; i--) {
-            level = paperAmount - paperUsed;
-            if (level == paperAmount) {
-                System.out.println("Your Paper tray is full");
+        int levelPaperUsed = 0;
+        for (int i = 0; i <= paperAmount ; i--) {
+            levelPaperUsed = paperAmount - paperUsed;
+            if (levelPaperUsed == paperAmount) {
+                System.out.println("single sided printing: Your Paper tray is full.");
                 break;
-            } else {
-                System.out.println("You have " + level + " pages left");
+            }
+            if (levelPaperUsed <= 0) {
+                System.out.println("Single sided printing: not enough supplies");
+            break;
+            }
+            else if (levelPaperUsed < paperAmount && levelPaperUsed > 0){
+                System.out.println("Single sided printing: You have: " + levelPaperUsed + " pages left");
                 break;
             }
 
 
+            }
+        return levelPaperUsed;
 
         }
-        return level;
+
+
+
+    public int twoSided(int twoSidedPrint) {
+        int levelDoublePrint = 0;
+        for (int i = 0; i <= paperAmount; i--) {
+            levelDoublePrint = paperAmount - (twoSidedPrint * 2);
+            if (levelDoublePrint < paperAmount) {
+                System.out.println("You have printed: " + levelDoublePrint + " pages using double sided.");
+                break;
+
+            }
+
+
+        }
+        return levelDoublePrint;
+
+
     }
 
+
+    public int twoSidedTonerUse(int tonerUsed) {
+        int levelDoubleToner = 0;
+        for (int i = 0; i <= paperAmount; i--) {
+            levelDoubleToner = tonerUsed * 2;
+            if (levelDoubleToner < paperAmount) {
+                System.out.println("You have: " + levelDoubleToner + "% toner remaining using double sided.");
+                break;
+            }
+
+
+        }
+        return levelDoubleToner;
+
+    }
+
+
+    public int addedPaper(int paperAdded) {
+        int levelPaperAdded = 0;
+        for (int i = 0; i <= paperAmount; i++) {
+            levelPaperAdded = paperAmount + paperAdded;
+            System.out.println("You have: " + levelPaperAdded + " pages remaining.");
+            break;
+        }
+
+
+        return levelPaperAdded;
+    }
 }
+
+
+
+
 
 
 
