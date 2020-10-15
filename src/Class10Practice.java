22bcf9ee9656ee2d728a546d07c4077852c778b6
David Smith <davidsmith@Davids-iMac.local>
2020-10-14 18:30:31 -0600

    Class10.java

--- src/Class10Practice.java
+++ src/Class10Practice.java
@@ -2,9 +2,11 @@ public class Class10Practice {
 
     public static void main(String[] args) {
         Class10 printingMethods = new Class10();
-         int singlePageToner = printingMethods.usedToner(91);
+        int singlePageToner = printingMethods.usedToner(91);
         int singPagePaper = printingMethods.usedPaper(99);
-
+        int doubleSidedPrint = printingMethods.twoSided(25);
+        int doubleSidedToner = printingMethods.twoSidedTonerUse(15);
+        int paperAdded = printingMethods.addedPaper(15);
 
 
 
